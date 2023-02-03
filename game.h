3719544597029fd54b7a2cd341572955151f2bc6
Rob Rose <robert.rose@live.com>
2023-02-03 16:20:48 +0000

    Update game.h

--- game.h
+++ game.h
@@ -1,23 +1,4 @@
-/*
-
-  The original version of Crazy Boulders by Dustan Strakl was programmed
-  in Basic see https://github.com/Dooshco/X16-CrazyBoulders. Dustan has
-  lots of useful information on programming the X16 see his readme file.
-
-  This C version is an exercise in writing a game in C using the cc65
-  compiler. Using Dustan's game design made it easer to consentrate on
-  the coding while learning how game mechanics work.
-
-  This programme uses the excelent libX16.lib by Chris Love to provide
-  vload and load_file functions to provide the C equivilent of the
-  basic commands VLOAD and LOAD.
-
-  This programme also uses the EFFECTS.PRG by Dustan Strakl to provide
-  sound effects.
-
-*/
-
-// tile defines
+// defines
 #define EMPTY_TILE            0
 #define PLAYER_TILE           1
 #define DIRT_FLOOR            2
